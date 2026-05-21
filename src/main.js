5fcbd5af968cc82b4efb668149c4eec0da4dafed
Abdulloh <abdulloh@example.com>
2026-05-21 16:25:15 +0500

    portfolio added

---
+++ src/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.multiple-text', {
+    strings: ["Frontend Developer", "YouTuber", "Blogger"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
